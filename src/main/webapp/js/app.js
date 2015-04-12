e4137d3b49f9fa602416aff66509034a7210f85c
Carlos Martínez <carlosmartcast@gmail.com>
2015-04-12 16:38:30 +0200

    frontend avanzado
    gui terminada a falta de integrar con la gui

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,4 +1,28 @@
+var SERVICE_URI = "/watershare/layer/";
+
 // A $( document ).ready() block.
 $( document ).ready(function() {
-  
+  mapInit();
+  hideSelections();
+  initSelector();
+  loadLayer('reservoirs', SERVICE_URI + 'reservoirs', 'red');
+  loadLayer('water_tanks', SERVICE_URI + 'water_tanks', 'blue');
+  //loadLayer('endpoints', SERVICE_URI + 'endpoints', 'green');
 });
+
+function hideSelections() {
+  $( '#heatmap-selected' ).hide();
+  $( ':checkbox' ).prop('checked', true);
+}
+
+function initSelector() {
+  $( '#samples-selected-opt' ).click(function() {
+    $( '#heatmap-selected' ).hide();
+    $( '#samples-selected' ).show();
+  });
+  
+  $( '#heatmap-selected-opt' ).click(function() {
+    $( '#samples-selected' ).hide();    
+    $( '#heatmap-selected' ).show();
+  });
+}
